7b2088153a68e09bcc3b6714cd8376ed203a10d9
Andrey Donskoy <donskoi.com@gmail.com>
2023-12-27 21:26:10 +0300

    add tasks

---
+++ lesson-06/homework/tasks/task_7.py
@@ -0,0 +1,23 @@
+import abc
+import asyncio
+
+
+class AbstractModel:
+    @abc.abstractmethod
+    def compute(self):
+        ...
+
+
+class Handler:
+    def __init__(self, model: AbstractModel):
+        self._model = model
+
+    async def handle_request(self) -> None:
+        # Модель выполняет некий тяжёлый код (ознакомьтесь с ним в файле тестов),
+        # вам необходимо добиться его эффективного конкурентного исполнения.
+        #
+        # Тест проверяет, что время исполнения одной корутины handle_request не слишком сильно
+        # отличается от времени исполнения нескольких таких корутин, запущенных конкурентно.
+
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, self._model.compute)
